eb551813b6726cee93eba799e894074a7f854c4a
poojah1998 <poojashreehazari37@gmail.com>
2024-01-13 17:40:40 +0530

    .

--- src/app/shared/recent-vendors/recent-vendors.component.ts
+++ src/app/shared/recent-vendors/recent-vendors.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 
-
 @Component({
   selector: 'app-recent-vendors',
   templateUrl: './recent-vendors.component.html',
@@ -9,48 +8,43 @@ import { ApiService } from '../../api.service';
 })
 export class RecentVendorsComponent implements OnInit {
   surveyorList: any[] = [];
+  allUserInfoData: any[] = [];
+
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.surveyorData();
   }
- 
+
   surveyorData() {
     this.api.getSurveyorsWithVendorInfo().subscribe(
       (res: any) => {
         this.surveyorList = res;
         console.log(this.surveyorList);
-        const allUserInfoData: any[] = [];
+
         const allVendorAccountInfoData: any[] = [];
+
         this.surveyorList.forEach(surveyor => {
-          surveyor.userInfoForSurveyor.forEach((userInfo :any) => {
-           console.log(userInfo,"userInfo..................")
+          surveyor.userInfoForSurveyor.forEach((userInfo: any) => {
             // Extract userInfoData and push to allUserInfoData array
             if (userInfo.userInfoData.length > 0) {
-              allUserInfoData.push(...userInfo.userInfoData);
-              console.log('All UserInfoData:', allUserInfoData);
+              this.allUserInfoData.push(...userInfo.userInfoData);
             }
-        
+
             // Extract vendorAccountInfoData and push to allVendorAccountInfoData array
-            console.log(userInfo.vendorAccountInfoData,"............userInfo.vendorAccountInfoData")
-            if (userInfo.vendorAccountInfoData) {
-              allVendorAccountInfoData.push(...userInfo.vendorAccountInfoData[0]);
-              console.log('All VendorAccountInfoData:', allVendorAccountInfoData);
+            if (userInfo.vendorAccountInfoData && userInfo.vendorAccountInfoData.length > 0) {
+              allVendorAccountInfoData.push(...userInfo.vendorAccountInfoData);
             }
           });
         });
-        
+
         // Now you have separate arrays for userInfoData and vendorAccountInfoData
-        console.log('All UserInfoData:', allUserInfoData);
+        console.log('All UserInfoData:', this.allUserInfoData);
         console.log('All VendorAccountInfoData:', allVendorAccountInfoData);
       },
       (error: any) => {
         console.error('Error getting vendors:', error);
       }
     );
-  
-
   }
-
-
 }
